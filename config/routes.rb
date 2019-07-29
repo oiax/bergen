0cec8ac11e32176f516eac6976d3e3fafe826e5d
Makoto Atarashi <makoto.atarashi0607@gmail.com>
2019-07-29 12:21:53 +0900

    新規投稿ページへのリンクを設置
    * config/routes.rbを書き換えた後、以下のコマンドを実行して新規投稿用の経路（new,
    create）が追加されていることを確認する。
    * bin/rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root "top#index"
   get "/messages", to: "messages#index", as: :messages
+  post "/messages", to: "messages#create"
+  get "/messages/new", to: "messages#new", as: :new_message
 end
