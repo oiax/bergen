ce999bc2f401a3d7722f25e9c27999704851ecb7
Makoto Atarashi <makoto.atarashi0607@gmail.com>
2019-08-20 18:33:30 +0900

    ログイン機能①（ユーザがいればログイン成功）
    * SessionsControllerを書き換え後に、ログインフォームからユーザ名に"alice"と入力してログインをクリックする。
    * サーバーを起動しているターミナル上で「ログイン成功！」という文字が表示されていることを確認する。
    * また、ブラウザではメッセージ一覧にリダイレクトされることを確認する。
    * 一方で、データベース上に存在しないユーザ名（"mike"など）を入力したり、空のままログインをクリックする。
    * その際はターミナル上に「ログイン失敗！」という文字が表示され、ブラウザ上でログインページに留まっていればOK。

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(name: params[:name])
+    if user
+      puts "ログイン成功！"
+      redirect_to messages_path
+    else
+      puts "ログイン失敗！"
+      render action: "new"
+    end
   end
 
   def destroy
