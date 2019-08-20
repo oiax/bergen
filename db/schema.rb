f24b2108dddd3b7dc50a9f86f6c8d5940f7c83bf
Makoto Atarashi <makoto.atarashi0607@gmail.com>
2019-08-20 17:22:19 +0900

    Userモデルとusersテーブルの作成
    * ターミナル上で以下のコマンドを実行する。
    * bin/rails g model User
    * 生成したマイグレーションファイルを書き換えた後にターミナル上で次のコマンドを実行する。
    * bin/rails db:migrate
    * なお、マイグレーションを実行すると自動生成される以下のファイルは書き換えてはならないので注意する。
    * db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_29_024701) do
+ActiveRecord::Schema.define(version: 2019_08_20_081930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "messages", force: :cascade do |t|
     t.string "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "hashed_password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
