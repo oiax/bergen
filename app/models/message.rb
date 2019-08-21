03c59dc0486225097385f307d676d00c877d9c3b
Makoto Atarashi <makoto.atarashi0607@gmail.com>
2019-08-21 11:27:30 +0900

    MessageモデルとUserモデルの関連付け
    * この書き換えにより、あるuserの投稿したmessageを以下のコードで取得することができるようになる。なお、*には任意のidが入る。
    * User.find(*).messages
    * また、あるmessageを投稿したuserを以下のコードで取得することができるようになる。上記同様に、*には任意のidが入る。
    * Message.find(*).user
    * なお、現時点ではシードデータでのUserとモデルの関連付けがされていないため、上記のコードをコンソール上で試してもnilしか得られない。

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ApplicationRecord
+  belongs_to :user
+  
   validates :content, presence: { message: "投稿内容を入力してください。" }
   validates :content, length: { maximum: 140, message: "140文字以内で入力してください。"}
 end
