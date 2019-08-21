b4623542894464008949dbbf1c9f1874d48c0fd1
Makoto Atarashi <makoto.atarashi0607@gmail.com>
2019-08-21 12:21:20 +0900

    シードデータの再投入（投稿内容と投稿者の紐付け）
    * 書き換えるシードデータ（db/seeds.rb）は自由にカスタマイズしてください。
    * その後、ターミナルで以下のコマンドを実行してブラウザで動作確認を行う。
    * bin/rails db:reset

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,18 @@
-m = Message.new
-m.content = "おはようございます！"
-m.save!
+# ユーザを作成
+user_names = %w(alice bob carol)
+user_names.each do |name|
+  User.create(name: name, password: "password")
+end
 
-m = Message.new
-m.content = "こんにちは！"
-m.save!
+# 作成したユーザを変数に保存
+alice = User.find_by(name: "alice")
+bob = User.find_by(name: "bob")
+carol = User.find_by(name: "carol")
 
-m = Message.new
-m.content = "こんばんは！"
-m.save!
-
-m = Message.new
-m.content = "おやすみなさい！"
-m.save!
-
-u = User.new
-u.name = "alice"
-u.password = "password"
-u.save!
-
-u = User.new
-u.name = "bob"
-u.password = "password"
-u.save!
-
-u = User.new
-u.name = "carol"
-u.password = "password"
-u.save!
+# ユーザ毎にメッセージを作成
+# ここは自由にカスタマイズしてください。
+Message.create(user: alice, content: "はじめまして、#{alice.name}です")
+Message.create(user: bob, content: "Railsの勉強頑張るぞー！")
+Message.create(user: carol, content: "#{bob.name}君には負けられない！")
+Message.create(user: alice, content: "このエラーは何で発生してるのかしら…")
+Message.create(user: bob, content: "少しずつ分かってきて楽しくなってきた！")
