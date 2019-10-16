45ee3bc0dd3809a8f84d274f7aa459d84112ceba
Makoto Atarashi <makoto.atarashi0607@gmail.com>
2019-10-16 15:30:10 +0900

    モデル間の関連付け③（has_many, through, source）
    * モデルファイルを書き換えた後、ターミナル上で以下のコマンドを順に打つ。
    
    * alice = User.find_by(name: "alice")
    * alice.followings
    * alice.followers
    
    * 2番目のコマンドではaliceがフォローしているbob、carolに関する情報を取得できることを確認する。
    * 3番目のコマンドではaliceをフォローしているbobに関する情報を取得できることを確認する。
    
    * この部分はRails開発において最難関レベルなので、以下にてこれらの動作イメージを整理する。
    * なお、本来的にはSQLのテーブル結合を利用しているため正確性には欠けるが、SQLの理解は比較的骨が折れるのでこちらでまずは概要を掴んでもらいたい。
    * まず、alice.followingsでは大まかに「中間テーブルであるrelationshipsテーブルを経由してusersテーブルにアクセスしている」と理解する。
    * そして、throughオプションを指定することで、この経由にはactive_relationshipsを利用することになり、<aliceがbobをフォロー>と<aliceがcarolをフォロー>という二つの情報（★）を経由することになる。
    * そして、source: :followerと指定することにより、★の情報からfollower_idとして指定されているbobとcarolのidが手に入り、このidを利用してUserテーブルからbobとcarolの情報を手に入れることができる。
    * alice.followersの動作イメージの理解は各自の宿題にする。

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :messages
   has_many :active_relationships, class_name: "Relationship", foreign_key: "following_id"
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "follower_id"
+  has_many :followings, through: :active_relationships, source: :follower
+  has_many :followers, through: :passive_relationships, source: :following
 
   def password=(raw_password)
     if raw_password.kind_of?(String)
