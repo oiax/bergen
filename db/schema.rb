8ea4802653ccca9777370a62b21ed70f1f94c2ae
Makoto Atarashi <makoto.atarashi0607@gmail.com>
2019-10-16 13:46:24 +0900

    フォロー・アンフォロー情報を格納するrelationshipsテーブルの作成
    * ターミナル上で以下のコマンドを実行する
    * bin/rails g model relationship
    * 生成されたマイグレーションファイルを書き換えたら、ターミナル上で以下のコマンドを実行する。
    * bin/rails db:migrate
    * 以下のファイルはマイグレーション実行後に自動で書き換えられため、変更しないこと。
    * db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_21_012106) do
+ActiveRecord::Schema.define(version: 2019_10_16_042203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 2019_08_21_012106) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "following_id"
+    t.integer "follower_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
