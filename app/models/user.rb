ce7853c95340769a5a12dfd568245d9f996aa3be
Makoto Atarashi <makoto.atarashi0607@gmail.com>
2019-10-16 14:25:40 +0900

    モデル間の関連付け②（has_many）
    * モデルファイルを書き換えた後、ターミナル上で以下のコマンドを順に打つ。
    
    * alice = User.find_by(name: "alice")
    * alice.active_relationships
    * alice.passive_relationships
    
    * 2番目のコマンドではrelationshipsテーブルに格納されているデータのうち、following_idがaliceのidと一致している（すなわち、aliceはbobとcarolをフォローしている）情報が得られることを確認する。
    * 3番目のコマンドではrelationshipsテーブルに格納されているデータのうち、follower_idがaliceのidと一致している（すなわち、aliceはbobにフォローされている）情報が得られることを確認する。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :messages
-  
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "following_id"
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "follower_id"
+
   def password=(raw_password)
     if raw_password.kind_of?(String)
       self.hashed_password = BCrypt::Password.create(raw_password)
