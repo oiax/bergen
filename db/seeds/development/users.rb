2fb9dfe7f145d96c8d782d3f524a5b1b9574ac7f
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-12-17 17:22:26 +0900

    usersテーブルにfull_nameとbioフィールドを追加

--- db/seeds/development/users.rb
+++ db/seeds/development/users.rb
@@ -1,5 +1,14 @@
 # ユーザを作成
-user_names = %w(alice bob carol)
-user_names.each do |name|
-  User.create(name: name, password: "password")
+users = [
+  ["alice", "鈴木アリス", "小説家。愛知県新城市生まれ。"],
+  [
+    "bob",
+    "佐藤ボブ",
+    "はーい。私の名前はボブです。最近プログラミングをはじめました。Railsの勉強をがんばりまーす。"
+  ],
+  ["carol", "高橋キャロル", "眠い。"]
+]
+
+users.each do |fields|
+  User.create(name: fields[0], full_name: fields[1], bio: fields[2], password: "password")
 end
