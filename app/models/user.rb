1f7953fa7f93c1fcdf9b1e045b0b92efc8b3b322
Makoto Atarashi <makoto.atarashi0607@gmail.com>
2020-01-27 15:57:33 +0900

    Userモデルのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
   has_many :followings, through: :active_relationships, source: :follower
   has_many :followers, through: :passive_relationships, source: :following
 
+  validates :name, :full_name, presence: { message: "入力必須項目です。" }
+  validates :name, uniqueness: { message: "既に使われています。" }
+
   def password=(raw_password)
     if raw_password.kind_of?(String)
       self.hashed_password = BCrypt::Password.create(raw_password)
