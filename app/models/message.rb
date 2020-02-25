080d4698333335a007064a477355c66293be6fd0
Makoto Atarashi <makoto.atarashi0607@gmail.com>
2020-02-25 22:06:10 +0900

    Messageモデルのインスタンスメソッドfavored_by?を定義

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,4 +5,8 @@ class Message < ApplicationRecord
 
   validates :content, presence: { message: "投稿内容を入力してください。" }
   validates :content, length: { maximum: 140, message: "140文字以内で入力してください。"}
+
+  def favored_by?(user)
+    user.in?(self.favors.map(&:user))
+  end
 end
