8f1cdcc7cf8b24215b9938bdc066a221135d87a1
Makoto Atarashi <makoto.atarashi0607@gmail.com>
2020-02-25 22:24:04 +0900

    FavorのDestroyのためのコントローラ実装

--- app/controllers/favors_controller.rb
+++ app/controllers/favors_controller.rb
@@ -8,5 +8,11 @@ class FavorsController < ApplicationController
   end
 
   def destroy
+    favor = Favor.find_by(
+      user: current_user,
+      message: Message.find(params[:message_id])
+    )
+    favor.destroy
+    redirect_back fallback_location: messages_path
   end
 end
