6df157dab84bac4210e476b77aafeec49894ecbc
Makoto Atarashi <makoto.atarashi0607@gmail.com>
2020-02-25 22:22:32 +0900

    FavorのCreateのためのコントローラ実装

--- app/controllers/favors_controller.rb
+++ app/controllers/favors_controller.rb
@@ -1,5 +1,10 @@
 class FavorsController < ApplicationController
   def create
+    favor = Favor.create(
+      user: current_user,
+      message: Message.find(params[:message_id])
+    )
+    redirect_back fallback_location: messages_path
   end
 
   def destroy
